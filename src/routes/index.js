7fdc509c8c96715a8f5ccc5300fbfb240d8fd6cc
cherced <Chernandezcediel@gmail.com>
2022-03-10 15:36:28 -0500

    testing of community proyect

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import Header from "../templates/Header";
 import Home from "../pages/Home";
 import Banner from "../templates/Banner";
+import Community from "../templates/Community";
 
 const routes = {
     "/": Home,
@@ -9,8 +10,11 @@ const routes = {
 const router = async () => {
     const header = null || document.getElementById("header");
     const banner = null || document.getElementById("banner");
+    const anime = null || document.getElementById("community");
     header.innerHTML = await Header();
     banner.innerHTML = await Banner();
+    community.innerHTML = await Community();
+    
 }
 
 export default router;
---
+++ src/templates/Community.js
@@ -0,0 +1,82 @@
+const Community = () => {
+    const view = `
+    <div class="scene">
+  <div class="plane">
+    <div class="cube cube--0">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--0"></div>
+    <div class="cube cube--1">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--1"></div>
+    <div class="cube cube--2">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--2"></div>
+    <div class="cube cube--3">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--3"></div>
+    <div class="cube cube--4">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--4"></div>
+    <div class="cube cube--5">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--5"></div>
+    <div class="cube cube--6">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--6"></div>
+    <div class="cube cube--7">
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+      <div class="cube__side"></div>
+    </div>
+    <div class="shadow shadow--7"></div>
+  </div>
+</div>`;
+    return view;
+}
+
+export default Community;
