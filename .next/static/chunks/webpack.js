3258e4b3707f57e666557b652de5c742c2c09450
cherced <Chernandezcediel@gmail.com>
2022-05-05 09:52:39 -0500

    StyleCorrection on IMG of Resume

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e7f910cd4600b06b"; }
+/******/ 		__webpack_require__.h = function() { return "85e45b1cfa370dd3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -36,7 +36,7 @@ const handleClickShowMenu = () => {
       <p>Frontend developer, with a degree in business administration, Croosfiter, lover of motorSports, and all the mecaninc staff, traveller, i belive on the continuous learning as fundamental principle of life , I have worked around 6 years in the administration of public civil infrastructure projects, with the pandemic I wanted to do a big change on my professional life, and I take the decision to make the a switch of professional career .</p>
       <p>I'm currently improving my technical skills in frontend technologies... HTML, CSS, JAVASCRIPT, a fan of Vercel and its Next.js framework, I am also currently in an intensive learning program to successfully meet that goal, his name is Platzi Master.</p>
       <p>my goal is to never stop learning, and if on this industry we have unicorns, I want to be the same pegasus!</p>
-      <p>Thank You For Reading</p>
+      <p>Thanks For Reading</p>
       <a onClick={() => (router.push('/VideoAboutMe'))}><button className="button" type="text">MORE</button></a>
     </div>
     <ul className={`menu_items ${menu ? "show" : ""}`}>
